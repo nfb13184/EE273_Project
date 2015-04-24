62023352ba2b6122619dadcbe44a7e1b3d8ee9a0
aahid <aahidshahid@ymail.com>
2015-04-24 23:38:05 +0100

    Update menu_functions.cpp

--- menu_functions.cpp
+++ menu_functions.cpp
@@ -105,7 +105,13 @@ void display_list_menu()
 	cout << "   Please choose a suitable option:\n";
 	int option2;
 	cin >> option2;
-	
+	while (cin.fail())
+	{
+		cout << "Invalid character.\n Please choose a valid option: \n";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> option2;
+	}
 	
 	
 
@@ -144,7 +150,7 @@ void display_list_menu()
 void displaying_from_file(string file_name){
 	food holder;
 	string line;
-	
+	system("cls");
 	infile.open(file_name);
 
 	//struct instance "holder" will keep the value of the food details from each line before it is written to the new file
@@ -159,12 +165,16 @@ void displaying_from_file(string file_name){
 		
 	}
 
-	
+	cout << "\n\n\n";
+	cout << setw(46) << "FOOD LIST\n\n\n";
+	cout << "----------------------------------------------------------------------------\n";
+	cout << setw(15) << "Food Name" << setw(15) << "Quantity" << setw(15) << "Cost" << setw(15) << "Expiry Date" << setw(15) << "Barcode\n";
+	cout << "----------------------------------------------------------------------------\n";
+
 	//until the end of file is reached, do this while loop
 	while(!infile.eof())
 	{
 		
-
 		getline(infile, line);
 
 		if (line.empty())
@@ -173,20 +183,14 @@ void displaying_from_file(string file_name){
 		}
 
 		holder = splitting_details_from_line(line);
-
-		//clear the console before the next item is shown
-		system("cls");
+		
 		//print the details of each item on to the console
-		cout << "         name: " << holder.food_name << endl;
-		cout << "         quantity: " << holder.quantity << endl;
-		cout << "         cost: \x9c " << holder.cost << endl;
-		cout << "         expiry date: " << holder.expiry_date << endl;
-		cout << "         barcode: " << holder.barcode << endl;
-		cout << "\n\n\n\n";
-		system("pause");
+		cout << setw(15) << holder.food_name << setw(12) << holder.quantity << setw(15) << "\x9c"<< holder.cost
+			<< setw(15) << holder.expiry_date << setw(15) << holder.barcode << endl;
+		
 		
 	}
-	cout << "\n\n\n         End of list     \n\n\n";
+	cout << "----------------------------------------------------------------------------\n";
 	system("pause");
 	infile.close();
 }
@@ -194,20 +198,27 @@ void displaying_from_file(string file_name){
 void search_menu(){
 
 	system("cls");
-	int option3;
+	int option3= NULL;
 	cout << "1. Search by name.\n";
 	cout << "2. Search by barcode\n";
-	cout << "3. Search by quantity.\n";
-	cout << "4. Search by expiry date.\n";
-	cout << "5. Back.\n";
+	cout << "3. Back.\n";
 	cout << "   Please choose an option from the menu:\n";
 	cin >> option3;
+	while (cin.fail())
+	{
+		cout << "Invalid character.\n Please choose a valid option: \n";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> option3;
+	}
 	
 	string line;
-	string name;
+	char name[100];
+	char comp =NULL ;
+	string name_holder;
+	int i;
+	int size;
 	string bc;
-	int quantity;
-	string expiry_date;
 	food holder;
 	int hit;
 	switch (option3){
@@ -217,6 +228,12 @@ void search_menu(){
 		//get the name the user wnats to search
 		cout << "Enter food name: \n";
 		cin >> name;
+		//to get the size
+		size = 0;
+		for (i = 0; i < 101; i++){
+			if (comp == name[i]){ }
+		}
+
 		//open the file that contains the food list
 		infile.open("updated_food_list.csv");
 
@@ -264,6 +281,7 @@ void search_menu(){
 		if (hit == 0){
 			cout << "No matches found, make sure the value is correct and try again\n";
 			system("pause");
+			break;
 		}
 		infile.close();
 		break;
@@ -298,7 +316,7 @@ void search_menu(){
 			{
 				continue;
 			}
-
+			hit = 0;
 			holder = splitting_details_from_line(line);
 			//compare each food item with the barcode
 			if (bc == holder.barcode){
@@ -314,133 +332,23 @@ void search_menu(){
 				break;
 			}
 			//if food item not found the variable hit will be set to 0
-			else{ hit = 0; }
+			//else if (bc != holder.barcode){ hit = 0; };
 		}
 		//message output to screen if the food item is not found
 		if (hit == 0){
 			cout << "No matches found, make sure the value is correct and try again\n";
 			system("pause");
+			break;
 		}
 		infile.close();
 		break;
-		
+
 	case 3:
-		//if the person wants to search by quantity
-		system("cls");
-		//get the quantity from user
-		cout << "Enter quantity: \n";
-		cin >> quantity;
-		//open file to be compared
-		infile.open("updated_food_list.csv");
-
-
-		if (!infile)
-		{
-			cout << "file cannot be located, make sure the file location is correct\n";
-			system("pause");
-			search_menu();
-
-		}
-
-
-		//until the end of file is reached, do this while loop
-		while (!infile.eof())
-		{
-
-
-			getline(infile, line);
-
-			if (line.empty())
-			{
-				continue;
-			}
-
-			holder = splitting_details_from_line(line);
-			//compare each food item with the barcode
-			if (quantity == holder.quantity){
-				//if food item found the variable hit will be set to 1
-				hit = 1;
-				cout << "Match found: \n";
-				cout << "            name: " << holder.food_name << endl;
-				cout << "            quantity: " << holder.quantity << endl;
-				cout << "            cost: \x9c " << holder.cost << endl;
-				cout << "            expiry date: " << holder.expiry_date << endl;
-				cout << "            barcode: " << holder.barcode << endl;
-				system("pause");
-				break;
-			}
-			//if food item not found the variable hit will be set to 0
-			else{ hit = 0; }
-		}
-		//message output to screen if the food item is not found
-		if (hit == 0){
-			cout << "No matches found, make sure the value is correct and try again\n";
-			system("pause");
-		}
-		infile.close();
-		break;
-
-	case 4:
-		//if the person wants to search by quantity
-		system("cls");
-		//get the quantity from user
-		cout << "Enter expiry date: \n";
-		cin >> expiry_date;
-		//open file to be compared
-		infile.open("updated_food_list.csv");
-
-
-		if (!infile)
-		{
-			cout << "file cannot be located, make sure the file location is correct\n";
-			system("pause");
-			search_menu();
-
-		}
-
-
-		//until the end of file is reached, do this while loop
-		while (!infile.eof())
-		{
-
-
-			getline(infile, line);
-
-			if (line.empty())
-			{
-				continue;
-			}
-
-			holder = splitting_details_from_line(line);
-			//compare each food item with the barcode
-			if (expiry_date == holder.expiry_date){
-				//if food item found the variable hit will be set to 1
-				hit = 1;
-				cout << "Match found: \n";
-				cout << "            name: " << holder.food_name << endl;
-				cout << "            quantity: " << holder.quantity << endl;
-				cout << "            cost: \x9c " << holder.cost << endl;
-				cout << "            expiry date: " << holder.expiry_date << endl;
-				cout << "            barcode: " << holder.barcode << endl;
-				system("pause");
-				break;
-			}
-			//if food item not found the variable hit will be set to 0
-			else{ hit = 0; }
-		}
-		//message output to screen if the food item is not found
-		if (hit == 0){
-			cout << "No matches found, make sure the value is correct and try again\n";
-			system("pause");
-		}
-		infile.close();
-		break;
-
-	case 5:
 		break;
 
 	default:cout << "\n\n Invalid option, please choose an appropriate option from the menu\n\n";
 		system("pause");
+		search_menu();
 		break;
 	}
 	if (option3 == 3){
@@ -458,13 +366,23 @@ food splitting_details_from_line(string line)
 		//if more information is added this loop counter will need to be increased
 		int counter;
 		int poss;
+		string name;
+		int size;
+		int i;
 		for (counter = 0; counter < 5; counter++){
 
 
 			if (counter == 0)
 			{
 				poss = line.find(marker);
-				holder.food_name = line.substr(0, poss);
+				name = line.substr(0, poss);
+				size = name.size();
+				for (i = 0; i < size; i++){
+					holder.food_name[i] = line.substr(0, poss)[i];
+				}
+				for (i = size; i < 100; i++){
+					holder.food_name[i] = NULL;
+				}
 				line = line.substr(poss + 1, line.length());
 
 			}
@@ -477,7 +395,7 @@ food splitting_details_from_line(string line)
 			if (counter == 2)
 			{
 				poss = line.find(marker);
-				holder.cost = stoi(line.substr(0, poss));
+				holder.cost = stof(line.substr(0, poss));
 				line = line.substr(poss + 1, line.length());
 			}
 
@@ -498,18 +416,101 @@ food splitting_details_from_line(string line)
 
 		return holder;
 }
+
+//contains the menu for editing
 void edit_list(){
+	/////////////////////////////////////////////////////BEFORE EDITING//////////////////////
+	///have to get the number of lines in the file so a vector of theat size can be made to store the food items
+	string line;
+	int number_of_lines;
+
+	//open the file that contains the food list
+	infile.open("updated_food_list.csv");
+
+
+	if (!infile)
+	{
+		cout << "file cannot be located, make sure the file location is correct\n";
+		system("pause");
+		search_menu();
+
+	}
+	//set the number of lines to zero to start off with
+	number_of_lines = 0;
+	//until the end of file is reached, do this while loop
+	while (!infile.eof())
+	{
+
+
+		getline(infile, line);
+
+		if (line.empty())
+		{
+			continue;
+		}
+
+		//increase the number for everyline read in
+		number_of_lines++;
+	}
+	//close the file after counting the number of lines
+	infile.close();
+
+	//create a dynamic vector, have to use pointer to make it dynamic memory
+	food* vc = new food[number_of_lines];
+	//////putting food info from each line into vectors
+	infile.open("updated_food_list.csv");
+
+	if (!infile)
+	{
+		cout << "file cannot be located, make sure the file location is correct\n";
+		system("pause");
+		search_menu();
+
+	}
+	//counter counts the up to the number of lines
+	int counter = 0;
+
+	//until the end of file is reached, do this while loop
+	while (!infile.eof())
+	{
+		//holder is where each line info will be  heald beofre put into the vector
+		food holder;
+
+		getline(infile, line);
+
+		if (line.empty())
+		{
+			continue;
+		}
+
+		while (counter < number_of_lines){
+			//each line is split into the food item details and put into the vector
+			holder = splitting_details_from_line(line);
+			vc[counter] = holder;
+			counter++;
+			break;
+		}
+	}
+	infile.close();
+	//////////////////////////////////////////////////////////BEFORE EDITING DONE////////////
 	//clear the console
 	system("cls");
 
 	//new menu for displaying lists
 	system("cls");
-	cout << "1. Delete food item.\n";
-	cout << "2. Edit entry.\n";
-	cout << "3. back.\n";
+	cout << "1. Delete food item\n";
+	cout << "2. Edit entry\n";
+	cout << "3. Back\n";
 	cout << "   Please choose a suitable option:\n";
 	int option3;
 	cin >> option3;
+	while (cin.fail())
+	{
+		cout << "Invalid character.\n Please choose a valid option: \n";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> option3;
+	}
 
 
 
@@ -518,11 +519,11 @@ void edit_list(){
 
 
 	case 1:
-		
+		delete_food(vc,number_of_lines);
 		break;
 
 	case 2:
-		
+		edit_item(vc, number_of_lines);
 		break;
 
 	case 3:
@@ -542,3 +543,37 @@ void edit_list(){
 	}
 }
 
+void delete_food(food *vector,int number_of_lines){
+	system("cls");
+	string delete_barcode;
+	//found variable is set to 1 if the item is found, and 0 otherwise
+	int found;
+	found = 0;
+	cout << "Which item do you want to delete? (Enter barcode):\n";
+	cin >> delete_barcode;
+
+
+	ofstream override;
+	override.open("updated_food_list.csv");
+	int counter;
+	for (counter = 0; counter < number_of_lines; counter++)
+	{
+		if (vector[counter].barcode == delete_barcode)
+		{
+			found = 1;
+			continue;
+		}
+
+		else
+		{
+			override << vector[counter].food_name << marker << vector[counter].quantity << marker << vector[counter].cost << marker << vector[counter].expiry_date << marker << vector[counter].barcode << marker << endl;
+		}
+
+	}
+	override.close();
+	if (found == 1){
+		cout << "\n\n Item Deleted\n";
+		system("pause");
+	}
+	else if (found == 0){ cout << "Item not found, make sure the barcode entered is correct and try again\n"; system("pause"); }
+}
